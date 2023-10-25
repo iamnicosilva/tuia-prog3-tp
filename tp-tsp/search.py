71153d24c96feebd6a1bdc54888c75941e93dd11
Bet <betsabe.g2@gmail.com>
2023-10-25 00:46:50 -0300

    busqueda tabu
    Signed-off-by: Bet <betsabe.g2@gmail.com>

--- tp-tsp/search.py
+++ tp-tsp/search.py
@@ -107,6 +107,7 @@ class HillClimbingReset(LocalSearch):
 
         # Arrancamos del estado inicial
         actual = problem.init
+
         #Valor objetivo
         value = problem.obj_val(problem.init)
 
@@ -114,14 +115,17 @@ class HillClimbingReset(LocalSearch):
         soluciones = {}
         restart_count = 0
 
+        # Tomamos un cuarto debido a que es mas performante
+        cant_reinicios = round(len(actual)*0.50)
+        print(cant_reinicios)
         while True:
 
             # Determinar las acciones que se pueden aplicar
             # y las diferencias en valor objetivo que resultan
             diff = problem.val_diff(actual)
             # print('dif: ',diff)
-            print('valor: ',value)
-            print(actual)
+            #print('valor: ',value)
+            #print(actual)
 
             # LINEA ORIGINAL:
             # Buscar las acciones que generan el mayor incremento de valor obj
@@ -149,7 +153,7 @@ class HillClimbingReset(LocalSearch):
             # (diferencia de valor objetivo no positiva)
 
             if diff[act] <= 0:
-                print('diff[act] no positivo: ',diff[act])
+                #print('diff[act] no positivo: ',diff[act])
                 soluciones[value] = actual
                 # ACA EN VEZ DE RETORNAR, TENEMOS QUE RESETEAR Y GUARDAR
                 # LOS VALORES DE ESTA PSEUDO-SOLUCIÓN PARA DESPUES 
@@ -161,7 +165,10 @@ class HillClimbingReset(LocalSearch):
 
             #Si no se reseteo 10 veces, se resetea y cuenta el reseteo
             # El contador 10 tiene que ser dinamico segun la cant de ciudades.
-                if restart_count < 5:
+
+            
+
+                if restart_count <= cant_reinicios:
                     actual = problem.random_reset()
                     value = problem.obj_val(actual) #value + diff[act]
                     restart_count +=1
@@ -176,29 +183,120 @@ class HillClimbingReset(LocalSearch):
 
                 end = time()
                 self.time = end-start
-
                 return
 
             # Sino, nos movemos al sucesor
             else:
 
-                print('diff act ',diff[act])
+                #print('diff act ',diff[act])
                 actual = problem.result(actual, act)
                 value = value + diff[act]
                 self.niters += 1
 
-
-
 class Tabu(LocalSearch):
     """Algoritmo de busqueda tabu."""
+  
     def solve(self, problem: OptProblem):
-        actual = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 0]
-        value = problem.obj_val(problem.init)
+        """Resuelve un problema de optimizacion con Búsqueda Tabú.
+
+        Argumentos:
+        ==========
+        problem: OptProblem
+            un problema de optimizacion
+        """
+        # Inicio del reloj
         start = time()
 
-        self.tour = actual
-        self.value = value
-        end = time()
-        self.time = end-start
+        # Arrancamos del estado inicial
+        actual = problem.init
+        mejor = actual
+        tabu = []
 
-        return
+        #Valor objetivo
+        value = problem.obj_val(problem.init)
+
+        # Inicializamos el contador de iteraciones
+        iter_count = 0
+        
+        while iter_count < 100:
+            no_tabu = {}
+            sucesores = {}
+            
+            # Determinar las acciones que se pueden aplicar
+            # y las diferencias en valor objetivo que re sultan
+            # Vecinos
+            posibles_acciones = problem.val_diff(actual)
+
+            #sucesores = problem.result(actual)
+
+            for accion in posibles_acciones:
+                # SUCESORES:
+                # La clave es el ACCION, asignamos primer atributo: lista del estado, segundo atributo: valor
+                # Clave: Accion
+                # Lista de Estados
+                # Valor    
+                sucesores[accion] = problem.result(actual,accion), posibles_acciones[accion]
+
+            #print(sucesores)
+
+            for sucesor in sucesores:
+                # Posición 0 = Lista de Estado
+                if sucesores[sucesor][0] not in tabu:
+                    #no_tabu.append(sucesor)
+                    no_tabu[sucesor] = sucesores[sucesor][0],sucesores[sucesor][1]
+                    print(no_tabu[sucesor][1])
+            
+            
+            #print('no tabu', no_tabu)
+
+            #max_value = max(no_tabu, key=no_tabu.get)
+            sucesor = max(no_tabu, key=lambda key: no_tabu[key][1])
+            print('sucesor',sucesor,no_tabu[sucesor])
+            ########################################################################
+            # SEGUIR POR ACA!!!!!!!!!!!!
+            #if problema.h(mejor) < problema.h(sucesor) then mejor ← sucesor 
+
+            #sucesor = no_tabu[max_value]
+            
+            #max_value = max(no_tabu.values())
+            
+
+            #if problem.val_diff(mejor) < problem.val_diff(sucesor)
+            # Iteramos a través de los elementos del diccionario diff
+            #for act, val in no_tabu.items():
+                # Comprobamos si el valor actual (val) es igual al valor máximo
+            #    if val == max_value:
+                    # Si es igual, agregamos la acción (act) a la lista max_acts
+            #        max_acts.append(act)
+
+            # Elegir una accion aleatoria
+            act = choice(max_acts)
+
+            # Retornar si estamos en un optimo local 
+            # (diferencia de valor objetivo no positiva)
+
+            if diff[act] <= 0:
+                #print('diff[act] no positivo: ',diff[act])
+                soluciones[value] = actual
+
+                if restart_count <= cant_reinicios:
+                    actual = problem.random_reset()
+                    value = problem.obj_val(actual) #value + diff[act]
+                    restart_count +=1
+                    continue
+            
+                mejor_solucion = max(soluciones)
+                self.tour = soluciones[mejor_solucion]
+                self.value = mejor_solucion 
+
+                end = time()
+                self.time = end-start
+                return
+
+            # Sino, nos movemos al sucesor
+            else:
+
+                #print('diff act ',diff[act])
+                actual = problem.result(actual, act)
+                value = value + diff[act]
+                self.niters += 1
